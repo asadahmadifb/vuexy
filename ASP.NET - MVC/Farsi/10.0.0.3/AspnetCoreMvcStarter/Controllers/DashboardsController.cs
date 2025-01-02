acf18e26bd30c1d91a55b83dd6c33c2929778334
asadahmadifb <asad.ahmadi.fb@gmail.com>
2025-01-02 19:53:00 +0330

    begin cf in starter project

---
+++ ASP.NET - MVC/Farsi/10.0.0.3/AspnetCoreMvcStarter/Controllers/DashboardsController.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreMvcFull.Controllers;
+
+public class DashboardsController : Controller
+{
+  public IActionResult Index() => View();
+  public IActionResult CRM() => View();
+  public IActionResult CF() => View();
+  public IActionResult CF2() => View();
+  public IActionResult Project() => View();
+  public IActionResult FinanceProvider() => View();
+  
+
+}
