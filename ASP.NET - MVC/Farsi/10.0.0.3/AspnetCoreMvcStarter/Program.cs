56e839248fea70ea7b23e7653be9027a0ab5ca16
asadahmadifb <asad.ahmadi.fb@gmail.com>
2025-01-05 12:33:50 +0330

    correct chat

--- ASP.NET - MVC/Farsi/10.0.0.3/AspnetCoreMvcStarter/Program.cs
+++ ASP.NET - MVC/Farsi/10.0.0.3/AspnetCoreMvcStarter/Program.cs
@@ -1,4 +1,13 @@
+using AspnetCoreMvcFull.Data;
+using AspnetCoreMvcFull.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Connect to the database
+builder.Services.AddDbContext<AspnetCoreMvcStarterContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("AspnetCoreMvcStarterContext") ?? throw new InvalidOperationException("Connection string 'AspnetCoreMvcFullContext' not found.")));
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -6,6 +15,13 @@ builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+// Create a service scope to get an AspnetCoreMvcFullContext instance using DI and seed the database.
+using (var scope = app.Services.CreateScope())
+{
+  var services = scope.ServiceProvider;
+
+  SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -24,6 +40,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=auth}/{action=LoginBasic}/{id?}");
+    //pattern: "{controller=auth}/{action=LoginBasic}/{id?}");
+    pattern: "{controller=home}/{action=index}/{id?}");
+
 
 app.Run();
